d606c10d771b27d0f0b3ed00a7d9f037e0aa4492
a4t <iwanomoto@gmail.com>
2015-11-30 14:54:29 +0900

    AutoScalingからEC2を普通に立てるように変更

--- terraform/tf/ec2_softether.tf
+++ terraform/tf/ec2_softether.tf
@@ -1,18 +1,31 @@
-# Launch Configuration 作成
-resource "aws_launch_configuration" "softether" {
-  name = "vpn_${var.envname}_softether"
-  image_id = "${var.ami_id.softether}"
+resource "aws_instance" "softether" {
+  ami = "${var.ami_id.softether}"
   instance_type = "${var.instance_type.softether}"
-  security_groups = [
+  iam_instance_profile = "${aws_iam_instance_profile.softether.name}"
+
+  subnet_id = "${aws_subnet.softether_server_zone1.id}"
+  vpc_security_group_ids = [
     "${aws_security_group.softether.id}",
     "${aws_security_group.developer_network.id}"
   ]
-  key_name = "private"
-  iam_instance_profile = "${aws_iam_instance_profile.softether.arn}"
-  associate_public_ip_address = true
+
   root_block_device = {
     volume_type = "gp2"
     volume_size = "20"
   }
+
   user_data = "${file("./userdatas/softether")}"
+  key_name = "${var.key_pair}"
+
+  tags {
+    Name = "vpn_${var.envname}_softether_server_zone1"
+    Service = "vpn"
+    Env = "${var.envname}"
+    Role = "softether_server_zone1"
+  }
+}
+
+resource "aws_eip" "softether_zone1_ip" {
+  instance = "${aws_instance.softether.id}"
+  vpc = true
 }
