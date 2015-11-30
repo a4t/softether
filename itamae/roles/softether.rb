02020a38fd8ea282fd5e68c4c9443d8d5c4a1018
a4t <iwanomoto@gmail.com>
2015-11-30 18:41:05 +0900

    起動時の流れがうまくいくように微調整

--- itamae/roles/softether.rb
+++ itamae/roles/softether.rb
@@ -1,3 +1,4 @@
 include_recipe "../cookbooks/common/default.rb"
 include_recipe "../cookbooks/softether/default.rb"
 include_recipe "../cookbooks/softether/users.rb"
+include_recipe "../cookbooks/softether/start.rb"
