70847cb94f49eda3031667559ed65fb059797022
a4t <iwanomoto@gmail.com>
2015-11-30 15:01:16 +0900

    Itamae実行まで

--- itamae/cookbooks/softether/default.rb
+++ itamae/cookbooks/softether/default.rb
@@ -5,16 +5,16 @@ node.reverse_merge!({
 })
 
 execute "Download Softether source" do
-  cwd "/usr/local/"
+  cwd node[:softether][:dir]
   command <<EOS
-    wget -O vpnserver.tar.gz http://jp.softether-download.com/files/softether/v4.19-9599-beta-2015.10.19-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.19-9599-beta-2015.10.19-linux-x64-64bit.tar.gz &&
+    wget -O vpnserver.tar.gz node[:softether][:repo] &&
     tar xzf vpnserver.tar.gz
 EOS
   not_if "test -e ./vpnserver.tar.gz"
 end
 
 execute "Download Softether source" do
-  cwd "/usr/local/vpnserver"
+  cwd "#{node[:softether][:dir]}/vpnserver"
   command <<EOS
     make i_read_and_agree_the_license_agreement && \
     chmod 600 * && chmod 700 vpncmd vpnserver
@@ -22,7 +22,7 @@ EOS
 end
 
 execute "Delete Softether source" do
-  cwd "/usr/local/"
+  cwd node[:softether][:dir]
   command "rm -rf vpnserver.tar.gz"
   only_if "test -e ./vpnserver.tar.gz"
 end
---
+++ itamae/roles/softether.rb
@@ -0,0 +1,2 @@
+include_recipe "../cookbooks/common/default.rb"
+include_recipe "../cookbooks/softether/default.rb"
