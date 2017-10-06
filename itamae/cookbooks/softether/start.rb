34c3005b64d71b71396f7aafa9f32200bd1af38c
a4t <iwanomoto@gmail.com>
2017-10-06 19:51:21 +0900

    output shared secret key

--- itamae/cookbooks/softether/start.rb
+++ itamae/cookbooks/softether/start.rb
@@ -1,5 +1,6 @@
 server_password = Digest::MD5.hexdigest(node[:softether][:master_password])[0, 20]
 psk = Digest::MD5.hexdigest(node[:softether][:psk])[0, 20]
+p "\n\nShared Secret Key = #{psk}\n\n"
 
 node[:softether][:hubs].each do |hubname, hubdata|
   hub_password = Digest::MD5.hexdigest(hubdata[:password])[0, 20]
